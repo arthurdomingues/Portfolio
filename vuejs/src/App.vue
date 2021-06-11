6bc8b10a3deca23bc59f4f3318f1b2ee5ec55e00
arthurdomingues <arthurdomingues76@gmail.com>
2021-06-11 17:11:59 -0300

    Vuejs-add router

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,37 +1,10 @@
 <template>
-<div>
-  <Header/>
-  <Welcome/>
-  <About/>
-  <Skills/>
-  <Footer/>
-</div>  
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-//import axios from 'axios';
-import Header from './components/Header/Header.vue'
-import Footer from './components/Footer/Footer.vue'
-import About from './components/About/About.vue'
-import Skills from './components/Skills/Skills.vue'
-import Welcome from './components/Welcome/Welcome.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Footer,
-    About,
-    Skills,
-    Welcome,
-  },
-  data(){
-    return{
-      hello: ''
-    }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -40,7 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  background-color: #e4e4e4;
+}
+
+#nav {
+  padding: 0px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ vuejs/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <Header/>
+    <Welcome/>
+    <About/>
+    <Skills/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header/Header.vue'
+import Welcome from '@/components/Welcome/Welcome.vue'
+import About from '@/components/About/About.vue'
+import Skills from '@/components/Skills/Skills.vue'
+import Footer from '@/components/Footer/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Welcome,
+    About,
+    Skills,
+    Footer,
+  }
+}
+</script>
