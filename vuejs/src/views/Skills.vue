c7cda15f04c50225023a8cd13192ce3bfafa6f5d
arthurdomingues <arthurdomingues76@gmail.com>
2021-06-13 17:10:39 -0300

    Vuejs-Skills view refactor

--- vuejs/src/views/Skills.vue
+++ vuejs/src/views/Skills.vue
@@ -31,13 +31,47 @@
 </template>
 
 <script>
-import dataCards from './dataCards'
 export default {
-  data(){
+  data: () => {
     return{
-      dataCard: dataCards,
-    }
-  }
+      dataCard: [
+        {
+          id:1,
+          image: 'node.png',
+          title: 'NodeJS',
+          content: 'Runtime Javascript',
+          href: 'https://nodejs.org/pt-br/'
+        },
+        {
+          id:2,
+          image: 'vue.png',
+          title: 'VueJS',
+          content: 'FrontEnd',
+          href: 'https://vuejs.org/'
+        },
+        {
+          id:3,
+          image: 'js.png',
+          title: 'Javascript',
+          content: 'Linguagem de Programação',
+          href: 'https://www.javascript.com/'
+        },
+        {
+          id: 4,
+          image: 'linux.png',
+          title: 'Linux',
+          content: 'Sistema operacional',
+          href: '#'
+        },
+        {
+          id: 5,
+          image: 'git.png',
+          title: 'Git',
+          content: 'Controle de Versionamento',
+          href: 'https://git-scm.com/'
+        }] 
+    }// end return
+  }//end data
 }
 </script>
 
