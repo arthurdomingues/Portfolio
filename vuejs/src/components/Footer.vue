e9040e0c10cad51d903544bf55841a95a1f4c58c
arthurdomingues <arthurdomingues76@gmail.com>
2021-06-13 12:40:02 -0300

    Vuejs-include font-awesome

--- vuejs/src/components/Footer.vue
+++ vuejs/src/components/Footer.vue
@@ -1,11 +1,16 @@
 <template>
-<footer class="footer">
-  <div class="content">
-    <p>
-        Escrito por Arthur Domingues
-    </p>
-    </div>
-</footer>
+<div class="social">
+  <a class="button is-medium is-twitter is-inverted">
+    <span class="icon">
+      <fa :icon="['fab', 'twitter']"></fa>
+    </span>
+  </a>
+  <a class="button is-medium is-github is-inverted">
+    <span class="icon">
+      <fa :icon="['fab', 'github']"></fa>
+    </span>
+  </a>
+</div>
 </template>
 
 <script>
--- vuejs/src/main.js
+++ vuejs/src/main.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "../node_modules/bulma/css/bulma.css"
+import "../node_modules/bulma-social/css/all.min.css"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import router from './router'
 
 
-//createApp(App).use(router).mount('#app')
+library.add(fas, faTwitter, faGithub)
 
 const app = createApp(App)
 app.use(router)
+app.component('fa', FontAwesomeIcon)
 app.mount('#app')
