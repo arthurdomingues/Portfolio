f3648b7a7e85b43ee22937f7920d6e700820f53d
arthurdomingues <arthurdomingues76@gmail.com>
2021-06-20 17:26:07 -0300

    vuejs-add 404 page

---
+++ vuejs/src/views/404.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="hero is-fullheight">
+    <div class="hero-body">
+      <div class="container is-centered">
+        <h1 class="is-size-1 has-text-left">404</h1>
+        <h2 class="is-size-3 has-text-left">Ops! A página não foi encontrada</h2>
+        <h2 class="is-size-3 has-text-left">
+          Você será redirecinado em breve
+        </h2>
+        <Notification ref="redirect" class="is-warning" :message="message"/>
+      </div>
+    </div>
+    <br>
+  </section>
+</template>
+
+<script>
+import Notification from '../components/Notification.vue'
+
+export default{
+  components:{
+    Notification
+  },
+  data(){
+    return{
+      message: 'Ops xP'
+    }
+  },
+  mounted: function(){
+    this.$refs.redirect.show()
+    setTimeout(()=>{
+        this.$router.push('/')
+      }, 3500)
+  }
+
+}
+</script>
+
+<style scoped>
+.hero{
+  background: #363636;
+}
+h1, h2{
+  color: white;
+}
+</style>
