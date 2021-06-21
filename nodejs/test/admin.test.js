3b7d598c8fbe579826119b2f851d93d7de406fd2
arthurdomingues <arthurdomingues76@gmail.com>
2021-06-21 11:18:52 -0300

    nodejs-add DB find name test on admin tests

--- nodejs/test/admin.test.js
+++ nodejs/test/admin.test.js
@@ -1,9 +1,20 @@
 const app = require('../index')
 const request = require('supertest')
 const jwt = require('jsonwebtoken')
+const AdminModel = require('../models/AdminUser')
+const assert = require('assert')
 require('dotenv').config()
 
-describe('Test Admin', async () => {
+describe('Test Admin Model', async () => {
+  it('DB Find Name', async () => {
+    /* Password not tested, because it is hash */
+    const name = process.env.ADMIN_NAME
+    const result = await AdminModel.find(name)
+    assert.strictEqual(result[0].name, name)
+  })
+})
+
+describe('Test Admin Controller', async () => {
   const admin = {
     name: process.env.ADMIN_NAME,
     password: process.env.ADMIN_PASSWORD
